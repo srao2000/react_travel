a05af19823280875ceefbb65625f1ef0511d46af
Sanjeev <sanjeevyadav3004@gmail.com>
2021-01-04 17:36:11 +0530

    Updates

---
+++ precache-manifest.9ae320366407cb1e110dc4e05e659f7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef999aac88fef497349cd72b9eab72a",
+    "url": "/react_travel/index.html"
+  },
+  {
+    "revision": "96db94ed6e8cbd47feb5",
+    "url": "/react_travel/static/css/main.73329814.chunk.css"
+  },
+  {
+    "revision": "c3f443d6bc71342caa86",
+    "url": "/react_travel/static/js/2.99799ddd.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react_travel/static/js/2.99799ddd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96db94ed6e8cbd47feb5",
+    "url": "/react_travel/static/js/main.f9ed7759.chunk.js"
+  },
+  {
+    "revision": "d9eb1004f12552e7c677",
+    "url": "/react_travel/static/js/runtime-main.77508ac9.js"
+  }
+]);
